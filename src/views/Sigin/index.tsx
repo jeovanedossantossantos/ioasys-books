a6b9882a5168ce549e207a7d3074c3642cc5f9af
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-29 16:09:14 -0300

    feat:  login creation

---
+++ src/views/Sigin/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Background } from "./style";
+import {FormSigin} from "../../components/FormSigin"
+export const Sigin: React.FC = () => {
+    return (
+
+        <Background>
+            <FormSigin/>
+        </Background>
+
+    )
+}
---
+++ src/views/Sigin/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Bg from '../../assets/background-image.png'
+export const Background = styled.div`
+
+    background-image: url(${Bg});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: fixed;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+          
+
+    @media (max-width: 768px){
+        background-position: center;
+    }
+    
+
+`

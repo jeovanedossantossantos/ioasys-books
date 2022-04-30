053f88c653ee304cd3f7c8bb439618e082af2c08
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-30 16:44:49 -0300

    feat: routes private

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,86 +1,46 @@
-import { useEffect, useState } from 'react'
-import jwt_decode from "jwt-decode";
-import { BrowserRouter, Routes as RoutesWrapper, Route, Navigate,  } from 'react-router-dom'
-import { Sigin } from '../views/Sigin'
-import { api } from '../service/api';
+
+import { useContext } from 'react';
+import { BrowserRouter, Routes as RoutesWrapper, Route, Navigate, useNavigate, } from 'react-router-dom'
+import { AuthContext } from '../hooks/auth';
 import { Home } from '../views/Home';
-import { Auth } from '../hooks/auth';
+import { Sigin } from '../views/Sigin'
+
 
 
 
 function Rotas() {
+    const {sigin,setSigin} = useContext(AuthContext)
 
-    const isLoggedIn: string | null = localStorage.getItem("@ioasysToken")
-    const auth=Auth()
-    const [isSectionActive, setIsSectionActive] = useState(auth)
-    // const push = useNavigate()
-
-    useEffect(() => {
-        setIsSectionActive(auth)
-
-    },[auth])
-
-    // const isSectionActive: any = async () => {
-    //     if (isLoggedIn === null) {
-    //         // console.log(isLoggedIn)
-    //         return false 
-    //     } else {
-    //         const onlYTokem: any = JSON.parse(isLoggedIn)
-    //         // const tokenPayload: any = jwt_decode(onlYTokem.refreshToken)
-    //         console.log(auth)
-    //         // try{
-    //         //     const res = await api.post("https://books.ioasys.com.br/api/v1/auth/refresh-token",{refreshToken:onlYTokem.refreshToken}) 
-    //         //     // console.log(res.status)
-    //         //     if(res.status===204){
-    //         //         return true;
-    //         //     }else{
-                   
-    //         //         return false;
-    //         //     }
-    //         // }catch(e){
-
-    //         // }
-    //         // const expSeconds = tokenPayload.exp
-    //         // const timeNow = Date.now() / 1000;
-    //         // return timeNow > expSeconds ? false : true;
-    //     }
-    // }
-
-    // useEffect(()=>{
-    //     isSectionActive()
-    // },[])
     return (
         <>
-        {/* <button onClick={() =>isSectionActive()}>clique</button> */}
-        <BrowserRouter >
-            <RoutesWrapper >
-            
-                <Route path="/" element={<Sigin/>}/>
-                {/* <Route path="/" element={<Home />}></Route> */}
-               
-                
-                {isSectionActive ? 
-                <Route path="/home" element={<Home />}></Route>:
-                 <Route path="/" element={<Navigate to="/"/>}/>
-                 }
-                {/* {!isSectionActive() &&
-                   <>
-                    <Route path="/formulario" element={
-                    <Formulario />} />
-                    <Route path="/lista" element={<Lista />}/>
-                   </>
-                } */}
-               
-                
+            {!sigin ?
+                <BrowserRouter >
+                    <RoutesWrapper >
 
-                {/* {!isSectionActive() ? 
-                <Route path="/formulario" element={<Navigate to="/"/>}/>: 
-                <Route path="/formulario" element={<Formulario/>}/>} */}
+                        <Route path="/" element={<Sigin />} />
+                        <Route
+                            path="*"
+                            element={<Navigate to="/" replace />}
+                        />
 
-                {/* <PrivateRoute path="/formulariio" component={<Formulario/>}></PrivateRoute> */}
-            </RoutesWrapper>
+                    </RoutesWrapper>
 
-        </BrowserRouter>
+                </BrowserRouter>
+                :
+                <BrowserRouter >
+                    <RoutesWrapper >
+
+
+                        <Route path="/" element={<Sigin />} />
+
+                        <Route path="/home" element={<Home />}></Route>
+                    </RoutesWrapper>
+
+                </BrowserRouter>
+
+            }
+
+
         </>
     )
 

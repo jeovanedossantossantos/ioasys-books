3af4411c2e5792684e17f3e2d2354b61ed93afe0
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-30 16:40:31 -0300

    chore: created authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Rotas from './routes/Routes';
-
+import AuthContext from './hooks/auth';
 function App() {
   return (
     < >
+      <AuthContext>
+        <Rotas />
+      </AuthContext>
 
-    <Rotas/>
-     
+
     </>
   );
 }
---
+++ src/hooks/auth.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
+
+type PropsSigin = {
+    sigin: boolean;
+    setSigin:React.Dispatch<
+    React.SetStateAction<boolean>>;
+    
+}
+
+const DEFAULT_VALUE = {
+  
+    sigin: false,
+    setSigin:()=>{},
+    
+    
+};
+
+export const AuthContext = createContext<PropsSigin>(DEFAULT_VALUE);
+
+const ContextProvider = ({children}: any) => {
+    const [sigin, setSigin] = useState(DEFAULT_VALUE.sigin)
+
+    return(
+        <AuthContext.Provider value={{sigin, setSigin}}>
+        
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default ContextProvider

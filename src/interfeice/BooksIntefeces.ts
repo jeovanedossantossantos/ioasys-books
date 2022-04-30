d1f0507134acb42709b4e432d3f346c91a7bc26b
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-30 14:27:26 -0300

    chore: create interfece of books

---
+++ src/interfeice/BooksIntefeces.ts
@@ -0,0 +1,14 @@
+export interface DataBooksProps {
+    authors: Array<string>,
+    category: string,
+    description: string,
+    id: string,
+    imageUrl: string,
+    isbn10: string,
+    isbn13: string,
+    language: string,
+    pageCount: number,
+    published: number,
+    publisher: string,
+    title: string,
+}

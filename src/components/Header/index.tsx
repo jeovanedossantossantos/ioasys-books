84848f5b5d866841d61d29cdef4763d4cb6d179d
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-30 14:26:06 -0300

    fix: bug of width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-
+import { LogoIoasys } from "../FormSigin/style";
+import Logo from "../../assets/img/Logo2.png"
+import { Nav } from "./style";
+import { FaSignOutAlt} from "react-icons/fa";
 export const Header: React.FC = () => {
-    return(
-        <nav>
-            <Link to="/">De</Link>
-        </nav>
+    const logOut = ()=>{
+        localStorage.removeItem("@ioasysToken")
+        localStorage.removeItem("@ioasysUser")
+
+    }
+    return (
+        <Nav>
+            <LogoIoasys>
+                <img src={Logo} alt="IOASYS" />
+                <h3>Books</h3>
+            </LogoIoasys>
+            <Link to="/" onClick={()=>logOut()}><FaSignOutAlt/></Link>
+        </Nav>
     )
 }
---
+++ src/components/Header/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const Nav = styled.nav`
+   
+    background: transparent;
+    height:120px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    overflow-x: hidden;
+
+    a{
+        display: flex;
+        font-size: 28px;
+        margin-bottom: 50px;
+        padding: 10px;
+        /* margin-right: 30px; */
+        background-color:#FFFFFF;
+    }
+`
